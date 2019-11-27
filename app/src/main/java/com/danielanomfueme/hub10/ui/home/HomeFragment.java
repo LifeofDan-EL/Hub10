48a6d177d5177d50ddf804646f5a8102a6e20b42
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-11-27 12:44:13 +0100

    Working on Home Fragment Layout

--- app/src/main/java/com/danielanomfueme/hub10/ui/home/HomeFragment.java
+++ app/src/main/java/com/danielanomfueme/hub10/ui/home/HomeFragment.java
@@ -11,19 +11,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.danielanomfueme.hub10.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView mRecyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.text_welcome);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
