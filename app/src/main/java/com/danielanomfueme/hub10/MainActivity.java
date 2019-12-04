0dd45a5431f8d18eab89614c3124bff0dcf08642
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-12-04 14:05:32 +0100

    Fixed Connect Nav header

--- app/src/main/java/com/danielanomfueme/hub10/MainActivity.java
+++ app/src/main/java/com/danielanomfueme/hub10/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_work, R.id.nav_train,
+                R.id.nav_home, R.id.nav_work, R.id.nav_train, R.id.nav_connect,
                 R.id.nav_about_us)
                 .setDrawerLayout(drawer)
                 .build();
