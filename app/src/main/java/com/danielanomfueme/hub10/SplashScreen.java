b4d454de173f7425f7aad6fd5147b6cb9050b194
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-12-04 12:01:46 +0100

    Implementing Splash Screen

--- app/src/main/java/com/danielanomfueme/hub10/SplashScreen.java
+++ app/src/main/java/com/danielanomfueme/hub10/SplashScreen.java
@@ -2,19 +2,27 @@ package com.danielanomfueme.hub10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
+    ImageView hub;
 
     private static int SPLASH_TIME_OUT  = 2000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        hub = findViewById(R.id.image_avatar);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -24,6 +32,11 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+
+        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(2000);
+        rotate.setInterpolator(new LinearInterpolator());
+        hub.startAnimation(rotate);
 
 
     }
